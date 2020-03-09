3f6b8986adff2d96692aed13a00871af426c4aec
Kevin Dupeyrat <kevin.dupeyrat@gmail.com>
2020-03-09 17:47:58 +0100

    Fix Eureka Config Microservices

--- ms1/src/main/java/com/microservice/ms1/Ms1Application.java
+++ ms1/src/main/java/com/microservice/ms1/Ms1Application.java
@@ -2,8 +2,12 @@ package com.microservice.ms1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties
 public class Ms1Application {
 
 	public static void main(String[] args) {
