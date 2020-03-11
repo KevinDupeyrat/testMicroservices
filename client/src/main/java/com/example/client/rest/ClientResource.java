39ea540d282e4a90b27f71e63dd732fd225576fe
Dupeyrat <kevin.dupeyrat@gmail.com>
2020-03-12 00:19:56 +0100

    Add Client

---
+++ client/src/main/java/com/example/client/rest/ClientResource.java
@@ -0,0 +1,14 @@
+package com.example.client.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClientResource {
+	
+	@RequestMapping
+	public String acceuil() {
+		return null;
+	}
+
+}
---
+++ zuul/src/main/java/com/example/zuul/config/LogFilter.java
@@ -0,0 +1,39 @@
+package com.example.zuul.config;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+
+		HttpServletRequest req = RequestContext.getCurrentContext().getRequest();
+
+		log.info("**** Requête interceptée ! L'URL est : {} ", req.getRequestURL());
+
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
